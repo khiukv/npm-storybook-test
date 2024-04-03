427bef261c5fa32bfcda4fbfa1b761e5160714ee
Vladimir VH. Hiuk <khiukv@gmail.com>
2024-04-03 11:06:31 +0200

    next commit

--- src/components/smartrating/SmartRating.stories.tsx
+++ src/components/smartrating/SmartRating.stories.tsx
@@ -1,12 +1,14 @@
 import { StoryFn, Meta } from "@storybook/react";
-import SmartRating from "./SmartRating";
+import {SmartRating} from "./SmartRating";
 
 export default {
   title: "ReactComponentLibrary/Rating",
   component: SmartRating,
 } as Meta<typeof SmartRating>;
 
-const Template: StoryFn<typeof SmartRating> = (args) => <SmartRating {...args} />;
+const Template: StoryFn<typeof SmartRating> = (args) => (
+  <SmartRating {...args} />
+);
 
 export const RatingTest = Template.bind({});
 RatingTest.args = {
