6935e0017a9371da8d5d51fbcc032e38d4a4b9ee
Vladimir VH. Hiuk <khiukv@gmail.com>
2024-04-08 14:44:47 +0200

    next commit

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,7 @@
 module.exports = {
-  "presets": [
-    "@babel/preset-env",
-    [
+    presets: [
+      "@babel/preset-env",
       "@babel/preset-react",
-      {
-        "runtime": "classic" // changed automatic to classic
-      }
+      "@babel/preset-typescript",
     ],
-    "@babel/preset-typescript"
-  ],
  };
